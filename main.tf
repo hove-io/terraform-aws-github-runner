9711f709e62e87de0596a1618763e2679093b9b3
ahattouti <abdel.hattouti@hove.com>
2025-11-10 11:48:51 +0100

    fix tag name

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ locals {
   tags = merge(var.tags, {
     "ghr:environment" = var.prefix
     "platfom"         = "landing-zone"
-    "solution"        = "github"
-    "component"       = "runner"
+    "solution"        = "hove"
+    "component"       = "github-runner"
     "terraform-stack" = "https://github.com/hove-io/terraform-aws-github-runner"
   })
 
